87f4e0647705f3d35530674174ee186844a83c15
Mohd Samir <84841504+Mohdsamiransari@users.noreply.github.com>
2023-05-28 10:55:47 +0530

    Update Url.js

--- src/components/Url.js
+++ src/components/Url.js
@@ -1,9 +1,9 @@
 
- export const URLS = "http://localhost:5000/"
+// export const URLS = "http://localhost:5000/"
 
 
 
 
-//export const URLS = "https://aris-backend.onrender.com/"
+export const URLS = "https://aris-backend.onrender.com/"
 
 
